d94170aec7e313a7a400ee67e721d8e0ff7c0736
gordonaa <aarongordon@g.ucla.edu>
2019-01-08 09:20:41 -0800

    add image

---
+++ code/TransitionMapping_function.R
@@ -0,0 +1,146 @@
+#Transition Mapping LCM#######################################
+#Jason Stein, Luis de la Torre Ubietta, Daniel Geschwind     #
+#                                                            #
+#                                                            #
+#See instructions                                            #
+##############################################################
+
+cat("\n\nTransition Mapping BrainSpan Data\n");
+cat(" by Jason Stein, Luis de la Torre Ubietta, Daniel Geschwind\n");
+cat(" Modified for RNAseq by Aaron Gorodn\n")
+cat("\n\n");
+
+color.bar <- function(lut, min, max=-min, nticks=11, ticks=seq(min, max, len=nticks), title='') {
+  scale = (length(lut)-1)/(max-min);  
+  plot(c(0,10), c(min,max), type='n', bty='n', xaxt='n', xlab='', yaxt='n', ylab='');
+  mtext(title,2,2.3,cex=0.8);
+  axis(2, round(ticks,0), las=1,cex.lab=0.8);
+  for (i in 1:(length(lut)-1)) {
+    y = (i-1)/scale + min;
+    rect(0,y,10,y+1/scale, col=lut[i], border=NA);
+  }
+}
+
+#Rank Rank Hypergeometric Overlap based on Plaisier et al., Nucleic Acids Research, 2010
+RRHO <- function(list1,list2,stepsize,labels,plots=FALSE,outputdir=NULL,annot=NULL) {
+  ## list 1 is a data.frame from experiment 1 with two columns, column 1 is the Gene Identifier, column 2 is the signed ranking value (e.g. signed -log10(p-value) or fold change)
+  ## list 2 is a data.frame from experiment 2 with two columns, column 1 is the Gene Identifier, column 2 is the signed ranking value (e.g. signed -log10(p-value) or fold change)
+  ## stepsize indicates how many genes to increase by in each algorithm iteration
+  #library(Vennerable);
+  
+  if (length(list1[,1])!=length(unique(list1[,1]))) 
+    stop('Non-unique gene identifier found in list1');
+  if (length(list2[,1])!=length(unique(list2[,1]))) 
+    stop('Non-unique gene identifier found in list2');
+  
+  list1 = list1[order(list1[,2],decreasing=TRUE),];
+  list2 = list2[order(list2[,2],decreasing=TRUE),];
+  nlist1 = length(list1[,1]);
+  nlist2 = length(list2[,1]);
+  
+  ## Number of genes on the array
+  N = max(nlist1,nlist2);
+  
+  hypermat = matrix(data=NA,nrow=length(seq(1,nlist1,stepsize)),ncol=length(seq(1,nlist2,stepsize)));
+  countx = county = 0;
+  ##Loop over the experiments
+  for (i in seq(1,nlist1,stepsize)) {
+    countx = countx + 1;
+    for (j in seq(1,nlist2,stepsize)) {
+      county = county + 1;
+      ## Parameters for the hypergeometric test
+      k = length(intersect(list1[1:i,1],list2[1:j,1]));
+      s = length(list1[1:i,1]);
+      M = length(list2[1:j,1]);
+      ## Hypergeometric test converting to log10
+      ## log10(x) = log_e(x) * log10(e)
+      hypermat[countx,county] = -phyper(k-1,M,N-M,s,lower.tail=FALSE,log.p=TRUE) * log10(exp(1));
+    }
+    county=0;
+  }
+
+  overlapmat = matrix(data=NA,nrow=length(seq(1,nlist1,stepsize)),ncol=length(seq(1,nlist2,stepsize)));
+  countx = county = 0;
+  ##Loop over the experiments
+  for (i in seq(1,nlist1,stepsize)) {
+    countx = countx + 1;
+    for (j in seq(1,nlist2,stepsize)) {
+      county = county + 1;
+      ## Parameters for the hypergeometric test
+      k = length(intersect(list1[1:i,1],list2[1:j,1]));
+      s = length(list1[1:i,1]);
+      ## Hypergeometric test converting to log10
+      ## log10(x) = log_e(x) * log10(e)
+      overlapmat[countx,county] = k/s;
+    }
+    county=0;
+  }
+  
+  ## Convert hypermat to a vector and Benjamini Yekutieli FDR correct
+  hypermatvec = matrix(hypermat,nrow=nrow(hypermat)*ncol(hypermat),ncol=1);    
+  hypermat.byvec = p.adjust(10^-hypermatvec,method="BY");
+  hypermat.by = matrix(-log10(hypermat.byvec),nrow=nrow(hypermat),ncol=ncol(hypermat));
+  
+  if (plots) {
+    ## Function to plot color bar
+    ## Modified from http://www.colbyimaging.com/wiki/statistics/color-bars
+    color.bar <- function(lut, min, max=-min, nticks=11, ticks=seq(min, max, len=nticks), title='') {
+      scale = (length(lut)-1)/(max-min);  
+      plot(c(0,10), c(min,max), type='n', bty='n', xaxt='n', xlab='', yaxt='n', ylab='');
+      mtext(title,2,2.3,cex=0.8);
+      axis(2, round(ticks,0), las=1,cex.lab=0.8);
+      for (i in 1:(length(lut)-1)) {
+        y = (i-1)/scale + min;
+        rect(0,y,10,y+1/scale, col=lut[i], border=NA);
+      }
+    }
+    
+    pdf(paste(outputdir,paste("RRHOMap",labels[1],"__VS__",labels[2],".pdf",sep=""),sep="/"));
+    jet.colors = colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan", "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"));
+    layout(matrix(c(rep(1,5),2), 1, 6, byrow = TRUE));
+    image(hypermat,xlab='',ylab='',col=jet.colors(100),axes=FALSE,main="Rank Rank Hypergeometric Overlap Map");
+    mtext(labels[2],2,0.5);
+    mtext(labels[1],1,0.5);
+    ##mtext(paste("-log10(BY P-value) =",max(hypermat.by)),3,0.5,cex=0.5);
+    color.bar(jet.colors(100),min=min(hypermat,na.rm=TRUE),max=max(hypermat,na.rm=TRUE),nticks=6,title="-log10(Nominal P-value)");
+    dev.off();
+
+    pdf(paste(outputdir,paste("OverlapMap",labels[1],"__VS__",labels[2],".pdf",sep=""),sep="/"));
+    jet.colors = colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan", "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"));
+    layout(matrix(c(rep(1,5),2), 1, 6, byrow = TRUE));
+    image(overlapmat,xlab='',ylab='',col=jet.colors(100),axes=FALSE,main="Rank Rank Percent Overlap Map");
+    mtext(labels[2],2,0.5);
+    mtext(labels[1],1,0.5);
+    color.bar(jet.colors(100),min=min(overlapmat,na.rm=TRUE),max=max(overlapmat,na.rm=TRUE),nticks=6,title="Overlap Percentage");
+    dev.off();
+    
+    ## Make a rank scatter plot
+    list2ind = match(list1[,1],list2[,1]);
+    list1ind = 1:nlist1;
+    corval = cor(list1ind,list2ind,method="spearman");
+    pdf(paste(outputdir,paste("RankScatter",labels[1],"__VS__",labels[2],".pdf",sep=""),sep="/"));
+    plot(list1ind,list2ind,xlab=paste(labels[1],"(Rank)"),ylab=paste(labels[2],"(Rank)"),pch=20,main=paste("Rank-Rank Scatter (rho = ",signif(corval,digits=3),")",sep=""),cex=0.5);
+    model = lm(list2ind~list1ind);
+    lines(predict(model),col="red",lwd=3);
+    dev.off();
+
+    ## Make a Venn Diagram for the most significantly associated points
+    ## Upper Right Corner (Downregulated in both)
+    maxind.ur = which(max(hypermat[ceiling(nrow(hypermat)/2):nrow(hypermat),ceiling(ncol(hypermat)/2):ncol(hypermat)],na.rm=TRUE)==hypermat,arr.ind=TRUE);
+    indlist1.ur = seq(1,nlist1,stepsize)[maxind.ur[1]];
+    indlist2.ur = seq(1,nlist2,stepsize)[maxind.ur[2]];
+    genelist.ur = intersect(list1[indlist1.ur:nlist1,1],list2[indlist2.ur:nlist2,1]);
+    ## Lower Right corner (Upregulated in both)
+    maxind.lr = which(max(hypermat[1:(ceiling(nrow(hypermat)/2)-1),1:(ceiling(ncol(hypermat)/2)-1)],na.rm=TRUE)==hypermat,arr.ind=TRUE);
+    indlist1.lr = seq(1,nlist1,stepsize)[maxind.lr[1]];
+    indlist2.lr = seq(1,nlist2,stepsize)[maxind.lr[2]];
+    genelist.lr = intersect(list1[1:indlist1.lr,1],list2[1:indlist2.lr,1]);
+
+    ## Write out the gene lists of overlapping
+    write.table(genelist.ur,paste(outputdir,"/RRHO_GO_MostDownregulated",labels[1],"__VS__",labels[2],".csv",sep=""),row.names=F,quote=F,col.names=F);
+    write.table(genelist.lr,paste(outputdir,"/RRHO_GO_MostUpregulated",labels[1],"__VS__",labels[2],".csv",sep=""),row.names=F,quote=F,col.names=F);
+    
+   
+  }  
+  return(list(hypermat=hypermat,hypermat.by=hypermat.by));
+}
---
+++ code/runRRHO.R
@@ -0,0 +1,105 @@
+# run RRHO comparing to brainSpan data
+
+options(stringsAsFactors = FALSE)
+args = commandArgs(trailingOnly = T)
+
+#load RRHO function
+source("code/TransitionMapping.R") 
+
+#load your_data data
+input_dir <- args[[1]] 
+if (!dir.exists(input_dir)) {
+  stop("input directory doesn't exist")
+}
+input_files <- list.files(input_dir, pattern = "csv")
+if (length(input_files) == 0) {
+  stop("input file is empty")
+} 
+test_data <- lapply(input_files, function(f) {
+  read.csv(f, header = T)
+})
+names(test_data) <- gsub(".csv$","", input_files)
+
+output_dir <- args[[2]]
+dir.create(output_dir, showWarnings = F,recursive = T)
+
+# load brainSPan data
+load("data/brainSpan_pariedVoom_results.rdata")
+
+stepsize <-  200
+
+# run RRHO
+for (j in c(1:length(brainSpanVoom))) {
+  ref <- as.data.frame(brainSpanVoom[[j]])
+  ref$geneID <- rownames(ref)
+  
+  for (i in c(1:length(test_data))) {
+    name_i <- names(test_data)[i]
+    name_j <- names(brainSpanVoom)[j]
+    print(paste(name_i,"......",name_j))
+    
+    test <- as.data.frame(test_data[[i]])
+    
+    ref <- ref[intersect(rownames(ref),rownames(test)),c("geneID","logFC")]
+    test <- test[intersect(rownames(ref),rownames(test)),c("geneID","logFC")]
+    
+    if (j == 1 & i == 1) {
+      hypermat.all = array(data = NA, 
+                           dim = c(length(test_data),
+                                   length(brainSpanVoom),
+                                   length(seq(1,length(ref[,1]),stepsize)),
+                                   length(seq(1,length(test[,1]),stepsize))))
+    }
+    
+    tmp <- RRHO(test,ref,stepsize = stepsize,labels = c(paste0("iPSC_DE_",name_i),paste0("brianSpan_DE_",name_j)),plots = F)
+    hypermat.all[i,j,,] = tmp$hypermat
+    
+  }
+}
+save(hypermat.all,file = paste0(outputFolder,"hypermatAll_",condition,".rdata"))
+
+#load(file=paste0(outputFolder,"hypermatAll_",condition,".rdata"))
+
+### Plot the output
+
+comparisons <- names(brainSpanVoom)
+nr <- length(unique(gsub("-.*","",comparisons)))
+nc <- length(unique(gsub(".*-","",comparisons)))
+
+jet.colors = colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan", "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"))
+colormap = jet.colors(100)
+
+minhypermat <-  min(hypermat.all,na.rm = TRUE)
+maxhypermat <-  max(hypermat.all,na.rm = TRUE)
+# for one comparison in the CIRMA data
+for (k in c(1:length(test_data))) {
+  transition <- names(test_data)[k]
+  png(paste0(outputFolder,transition,"_brainSpan_RRHOMap.png"),width = 10, height = 10, units = "in", res = 300)
+  par(mfrow = c(nr + 1, nc + 1),
+    mar = c(0, 0, 0, 0),
+    oma = c(0, 0, 0, 0))
+  count = 1
+  for (j in 1:(nr + 1)) {
+    for (i in 1:(nc + 1)) {
+      if (j < i) {
+        ## Scale the output to the max across all hypermats so that the colorbar is the same for all hypermats
+        image(hypermat.all[k, count, , ],
+              xlab = '',
+              ylab = '',
+              axes = FALSE,
+              col = colormap, 
+              zlim = c(minhypermat,maxhypermat))
+        count = count + 1
+      } else {
+        plot(0,type = "n", axes = F, ylab = '', xlab = '')
+      }
+    }
+  }
+  dev.off()
+}
+## Function to plot color bar
+## Modified from http://www.colbyimaging.com/wiki/statistics/color-bars
+png(paste0(outputFolder,"brainSpan_RRHOMap_color_bar.png",sep = ""), width = 2, height = 6, units = "in", res = 300)
+color.bar(jet.colors(100),min = minhypermat, max = maxhypermat, nticks =  6, title = "-log10(Nominal P-value)")
+dev.off()
+
